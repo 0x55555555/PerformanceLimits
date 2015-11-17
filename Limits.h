88f90f3ff1365efe220559777e529b3281c35474
George Pimm <jorjpimm@gmail.com>
2015-11-17 21:48:02 +0000

    results capturing

--- Limits.h
+++ Limits.h
@@ -6,145 +6,132 @@
 
 namespace limits
 {
-  
+
 struct TestParameters
 {
-  std::size_t size;
+  std::size_t count;
 };
-  
+
 inline std::ostream &operator<<(std::ostream &str, const TestParameters &p)
 {
-  return str << "{ " << p.size << " }";
+  return str << "{ " << p.count << " }";
 }
-  
+
 class TestBase
 {
 public:
   enum Type
   {
-    Sized
+    Sized,
+    Count
   };
-  
+
   struct Setup
   {
     std::string unit = "";
   };
-  
-  TestBase(const char *name, Type t)
-  : m_name(name)
-  , m_type(t)
-  {
-  }
-  
-  const std::string &name() const { return m_name; }
-  
-  void callback()
-  {
-    if (m_type == Sized) {
-      TestParameters p;
-      
-      p.size = 1;
-      capture_result(p);
-      
-      p.size = 10;
-      capture_result(p);
-      
-      p.size = 100;
-      capture_result(p);
-      
-      p.size = 100;
-      capture_result(p);
-      
-      p.size = 1000;
-      capture_result(p);
-      
-      p.size = 1000;
-      capture_result(p);
-    }
-  }
-  
-  void capture_result(const TestParameters &p)
-  {
-    Result result;
-    result.parameters = p;
-    m_current_result = &result;
-  
-    try
-    {
-      callback_impl();
-    }
-    catch(...)
-    {
-      m_current_result = nullptr;
-      throw;
-    }
-  
-    m_results.push_back(result);
-  
-  
-    double value = result.parameters.size;
-    double time = result.time.count();
-    time /= 1e9;
-  
-    auto rate = value / time;
-  
-    std::cout << "Captured result " << p << "\n" <<
-      "  with time " << result.time.count() << "ns\n" <<
-      "  rate " << rate << " " << m_setup.unit << "/s" << std::endl;;
-  }
-  
-  virtual void callback_impl() = 0;
-  
-  template <typename T> void run(const T &t)
-  {
-    auto begin = std::chrono::high_resolution_clock::now();
-    t();
-    auto end = std::chrono::high_resolution_clock::now();
-  
-    m_current_result->time = end - begin;
-  }
-  
-  const TestParameters &parameters()
-  {
-    return m_current_result->parameters;
-  }
-  
-  Setup &setup()
-  {
-    return m_setup;
-  }
-  
-private:
-  std::string m_name;
-  Type m_type;
-  Setup m_setup;
   
   struct Result
   {
     TestParameters parameters;
     std::chrono::nanoseconds time;
   };
+
+  TestBase(const char *name, Type t)
+  : m_name(name)
+  , m_type(t)
+  {
+  }
+
+  const std::string &name() const { return m_name; }
+
+  void callback()
+  {
+    TestParameters p;
+
+    const std::chrono::milliseconds minimum(100);
+
+    p.count = 1;
+    do {
+      capture_result(p);
+      p.count *= 10;
+    } while(m_results.back().time < minimum);
+  }
+
+  void capture_result(const TestParameters &p)
+  {
+    Result result;
+    result.parameters = p;
+    m_current_result = &result;
+
+    try
+    {
+      if (m_type == Sized) {
+        callback_impl();
+      }
+      else {
+
+        callback_impl();
+      }
+    }
+    catch(...)
+    {
+      m_current_result = nullptr;
+      throw;
+    }
+
+    m_results.push_back(result);
+  }
+
+  virtual void callback_impl() = 0;
+
+  template <typename T> void run(const T &t)
+  {
+    auto begin = std::chrono::high_resolution_clock::now();
+    t();
+    auto end = std::chrono::high_resolution_clock::now();
+
+    m_current_result->time = end - begin;
+  }
+
+  const TestParameters &parameters()
+  {
+    return m_current_result->parameters;
+  }
+
+  Setup &setup()
+  {
+    return m_setup;
+  }
+
+  const Result &final_result() { return m_results.back(); }
+
+private:
+  std::string m_name;
+  Type m_type;
+  Setup m_setup;
+
   std::vector<Result> m_results;
   Result *m_current_result;
 };
-  
+
 class Tests
 {
 public:
   Tests(int argc, char **argv)
   {
   }
-  
+
   static void install(TestBase *t)
   {
     m_tests.push_back(t);
   }
-  
+
   int run()
   {
     std::cout << "Running all tests" << std::endl;
     bool all_success = true;
-  
+
     for (auto &t : m_tests)
     {
       std::cout << "  Running test " << t->name() << std::endl;
@@ -157,27 +144,51 @@ public:
         std::cerr << "    ... failed in test" << std::endl;
         all_success = false;
       }
-    
+
+      double value = t->final_result().parameters.count;
+      double time = t->final_result().time.count();
+      auto time_unit = "ns";
+      if (time > 1000000000) {
+        time_unit = "s";
+        time /= 1e9;
+      }
+      if (time > 1000000) {
+        time_unit = "us";
+        time /= 1e6;
+      }
+      if (time > 1000) {
+        time_unit = "ms";
+        time /= 1e3;
+      }
+
+      auto rate = value / time;
+
+      std::cout << "  Captured result with time " << time << time_unit << "\n" <<
+      "    rate " << rate << " " << t->setup().unit << "/" << time_unit << std::endl;;
+
       std::cout << "  ... complete" << std::endl;
     }
-  
+
     std::cout << "... all tests complete" << std::endl;
     return all_success ? 0 : 1;
   }
-  
+
   static std::vector<TestBase *> m_tests;
 };
 
 }
 
-#define LIMITS_TEST_NAME() Test##__LINE__
+#define LIMITS_JOIN2(a, b) a ## b
+#define LIMITS_JOIN(a, b) LIMITS_JOIN2(a, b)
+#define LIMITS_TEST_NAME LIMITS_JOIN(Test, __LINE__)
+
 #define LIMITS_TEST(name, type) \
-  struct LIMITS_TEST_NAME() : \
+  struct LIMITS_TEST_NAME : \
     public limits::TestBase { \
-      LIMITS_TEST_NAME()() : TestBase(#name, limits::TestBase::type) \
+      LIMITS_TEST_NAME() : TestBase(#name, limits::TestBase::type) \
         { limits::Tests::install(this); } \
       void callback_impl() override; }; \
-  LIMITS_TEST_NAME() g_##LIMITS_TEST_NAME; \
-  void LIMITS_TEST_NAME()::callback_impl()
+  LIMITS_TEST_NAME LIMITS_JOIN(g_, LIMITS_TEST_NAME); \
+  void LIMITS_TEST_NAME::callback_impl()
 
 #define LIMITS_APP() int main(int argc, char **argv) { limits::Tests tests(argc, argv); return tests.run(); }
--- MemCpy.cpp
+++ MemCpy.cpp
@@ -1,19 +1,81 @@
 #include "Limits.h"
 #include <array>
+#include <cmath>
 
-LIMITS_TEST(thing, Sized) {
+LIMITS_TEST(memcpy, Sized) {
   
   auto p = parameters();
-  
   setup().unit = "MB";
   
   using Megabyte = std::array<char, 1000 * 1000>;
   
-  std::vector<Megabyte> a(p.size);
-  std::vector<Megabyte> b(p.size);
+  std::vector<Megabyte> a(p.count);
+  std::vector<Megabyte> b(p.count);
   
   run([&]() {
     memcpy(b.data(), a.data(), a.size() * sizeof(*a.data()));
+  });
+}
+
+LIMITS_TEST(multiply, Count) {
+  
+  auto p = parameters();
+  setup().unit = "operation";
+  
+  std::vector<int> a(p.count);
+  std::vector<int> b(p.count);
+  std::vector<int> c(p.count);
+  
+  run([&]() {
+    for (std::size_t i = 0; i < a.size(); ++i) {
+      c[i] = a[i] * b[i];
+    }
+  });
+}
+
+LIMITS_TEST(complex, Count) {
+  
+  auto p = parameters();
+  setup().unit = "operation";
+  
+  std::vector<int> a(p.count);
+  std::vector<int> b(p.count);
+  std::vector<int> c(p.count);
+  std::vector<int> d1(p.count);
+  std::vector<int> d2(p.count);
+  
+  run([&]() {
+    for (std::size_t i = 0; i < a.size(); ++i) {
+      auto a_ = a[i];
+      auto b_ = b[i];
+      auto c_ = c[i];
+      
+      d1[i] = (-b_ + (b_ * b_ - 4 * a_ * c_)) / 2 * a_;
+      d2[i] = (-b_ - (b_ * b_ - 4 * a_ * c_)) / 2 * a_;
+    }
+  });
+}
+
+LIMITS_TEST(sqrt, Count) {
+  
+  auto p = parameters();
+  setup().unit = "operation";
+  
+  std::vector<int> a(p.count);
+  std::vector<int> b(p.count);
+  std::vector<int> c(p.count);
+  std::vector<int> d1(p.count);
+  std::vector<int> d2(p.count);
+  
+  run([&]() {
+    for (std::size_t i = 0; i < a.size(); ++i) {
+      auto a_ = a[i];
+      auto b_ = b[i];
+      auto c_ = c[i];
+      
+      d1[i] = (-b_ + sqrt(b_ * b_ - 4 * a_ * c_)) / 2 * a_;
+      d2[i] = (-b_ - sqrt(b_ * b_ - 4 * a_ * c_)) / 2 * a_;
+    }
   });
 }
 
